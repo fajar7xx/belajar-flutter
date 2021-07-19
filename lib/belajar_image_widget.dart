9ba76eac9a8fa5b0522e983cf9520a5c849ccc58
Fajar Siagian <fajar7xx@gmail.com>
2021-07-19 10:22:32 +0700

    spacer widget

---
+++ lib/belajar_image_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarImageWidget());
+
+class BelajarImageWidget extends StatelessWidget {
+  const BelajarImageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Belajar Image Widget"),
+          ),
+          body: Center(
+              child: Container(
+            color: Colors.black87,
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            // ambil dari network atau api
+            // child: Image(
+            //   image: NetworkImage(
+            //       "https://cdn.pixabay.com/photo/2016/12/25/22/08/christmas-1931054_960_720.jpg"),
+            //   fit: BoxFit
+            //       .contain, //menampilkan gambar sesuai proporsi/responsive terhadap container
+            //   // repeat: ImageRepeat.repeat,
+            // ), //ambil dari internet
+            //ambil dari asset
+            child: Image(
+              image: AssetImage("assets/boy.jpg"),
+              fit: BoxFit.contain,
+              repeat: ImageRepeat.noRepeat,
+            ),
+          ))),
+    );
+  }
+}
---
+++ lib/belajar_spacer_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarSpacerWidget());
+
+class BelajarSpacerWidget extends StatelessWidget {
+  const BelajarSpacerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("belajar spacer widget"),
+            ),
+            body: Center(
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Container(
+                    width: 80,
+                    height: 80,
+                    color: Colors.red,
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Container(
+                    width: 80,
+                    height: 80,
+                    color: Colors.blue,
+                  ),
+                  Spacer(
+                    flex: 3,
+                  ),
+                  Container(
+                    width: 80,
+                    height: 80,
+                    color: Colors.green,
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                ],
+              ),
+            )));
+  }
+}
