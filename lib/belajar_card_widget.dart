3a29ae0557fe562c97425464eeae8a9c4741e4b9
Fajar Siagian <fajar7xx@gmail.com>
2021-07-23 14:25:09 +0700

    textfield

---
+++ lib/belajar_card_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarCardWidget());
+
+class BelajarCardWidget extends StatelessWidget {
+  const BelajarCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.green,
+            body: Container(
+              margin: EdgeInsets.all(10),
+              child: ListView(
+                children: <Widget>[
+                  buildCard(Icons.account_box, "Account Box"),
+                  buildCard(Icons.adb, "Android Test")
+                ],
+              ),
+            )));
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      // elevation => jarak objek daengan bg
+      elevation: 5,
+      child: Row(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.all(5),
+              child: Icon(iconData, color: Colors.green)),
+          Text(text)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/belajar_textfield.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarTextField());
+
+class BelajarTextField extends StatefulWidget {
+  const BelajarTextField({Key? key}) : super(key: key);
+
+  @override
+  _BelajarTextFieldState createState() => _BelajarTextFieldState();
+}
+
+class _BelajarTextFieldState extends State<BelajarTextField> {
+  // controller untuk textfield
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Latihan Text Field"),
+          ),
+          body: Container(
+            margin: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                TextField(
+                  // obscureText: true, //inputan untuk password
+                  // maxLength: 5, //panjang karakter
+                  // maxLines: 2, //maximal brapa baris
+                  decoration: InputDecoration(
+                      fillColor: Colors.lightBlue[50],
+                      filled: true,
+                      icon: Icon(Icons.adb), //icon di luar
+                      prefix: Container(
+                        width: 5,
+                        height: 5,
+                        color: Colors.blue,
+                      ),
+                      // kalau pasang prefix gak boleh ada prefix di bawah ini
+                      // prefixText: "Name: ", //untuk placeholder
+                      // prefixStyle: TextStyle(
+                      //     color: Colors.blue, fontWeight: FontWeight.bold),
+                      prefixIcon: Icon(Icons.person), //icon di dalam border
+                      labelText: "Nama Lengkap",
+                      labelStyle: TextStyle(color: Colors.black54),
+                      hintText: "Nama Lengkap sesuai KTP/Identitas",
+                      hintStyle: TextStyle(fontSize: 12),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                  controller: controller,
+                ),
+                Text(controller.text)
+              ],
+            ),
+          )),
+    );
+  }
+}
