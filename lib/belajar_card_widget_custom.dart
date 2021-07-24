512771fd266bee8d6c14e7f14f6e6f9ec2c14749
Fajar Siagian <fajar7xx@gmail.com>
2021-07-24 15:31:48 +0700

    23 ok

---
+++ lib/belajar_card_widget_custom.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarCardWidgetCustom());
+
+class BelajarCardWidgetCustom extends StatelessWidget {
+  const BelajarCardWidgetCustom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPageSecond(),
+    );
+  }
+}
+
+class MainPageSecond extends StatelessWidget {
+  const MainPageSecond({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Custom Card Example",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF8C062F),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xFFFE5788), Color(0xFFF56D5D)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 10,
+                child: Stack(
+                  children: <Widget>[
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                                4), //nilai default dari card
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png"),
+                                fit: BoxFit.cover)),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(4),
+                              topRight: Radius.circular(4)),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://cdn.pixabay.com/photo/2015/03/26/09/48/buildings-690364_960_720.jpg"),
+                              fit: BoxFit.cover)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          20,
+                          50 + MediaQuery.of(context).size.height * 0.35,
+                          20,
+                          20),
+                      child: Center(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "Sunset di Perkotaan Metropolitan",
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color(0xFFF56D5D), fontSize: 25),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      "Posted On  ",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    ),
+                                    Text(
+                                      "July 24, 2021",
+                                      style: TextStyle(
+                                          color: Color(0xFFF56D5D),
+                                          fontSize: 16),
+                                    ),
+                                  ]),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Spacer(
+                                  flex: 10,
+                                ),
+                                // icon
+                                Icon(
+                                  Icons.thumb_up,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                // icon
+                                Text(
+                                  "99",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 5,
+                                ),
+                                // icon
+                                Icon(
+                                  Icons.comment,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                //Text
+                                Text(
+                                  "888",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 10,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
